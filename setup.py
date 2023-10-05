1fa0d5d41401a2531690c568a4936285eea36b87
MartinKlevs <31007480+MartinKlevs@users.noreply.github.com>
2023-10-05 17:38:13 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
 setup(
-        name='MOSESDMD',
+        name='MOSESSVD',
         version='1.0',
         maintainer="Martins Klevs",
         url="https://github.com/MartinKlevs/MOSES-SVD/",
         keywords=["svd", "streaming-svd", "online-svd"],
-        packages=['MOSESDMD'],
+        packages=['MOSESSVD'],
         install_requires=["numpy", "scipy", "numba"],
         licence="Unlicence",
      )
