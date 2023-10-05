2f4b9a1303487734b715a8c8eecdc169859c5eaf
MartinKlevs <31007480+MartinKlevs@users.noreply.github.com>
2023-10-05 17:35:07 +0300

    Update setup.py
    Added missing commas in setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from setuptools import setup
 
-setup(name='MOSESDMD',
+setup(
+        name='MOSESDMD',
         version='1.0',
         maintainer="Martins Klevs",
-        url="https://github.com/MartinKlevs/MOSES-SVD/"
-        keywords=["svd", "streaming-svd", "online-svd"]
+        url="https://github.com/MartinKlevs/MOSES-SVD/",
+        keywords=["svd", "streaming-svd", "online-svd"],
         packages=['MOSESDMD'],
-        install_requires=["numpy", "scipy", "numba"]
-        licence="Unlicence"
+        install_requires=["numpy", "scipy", "numba"],
+        licence="Unlicence",
      )
